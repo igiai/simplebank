27b80d5137599eea2ff09098b8ddaaa1cdd2e317
Michał Bernacki <michber575@gmail.com>
2023-04-18 14:38:35 +0200

    Use Viper for config

--- main.go
+++ main.go
@@ -6,26 +6,26 @@ import (
 
 	"github.com/igiai/simplebank/api"
 	db "github.com/igiai/simplebank/db/sqlc"
+	"github.com/igiai/simplebank/db/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5433/simplebank_db?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("cannot connect to db:", err)
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
-		log.Fatal("cannot start server", err)
+		log.Fatal("cannot start server: ", err)
 	}
 }
