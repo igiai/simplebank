c84082db91cb1c41fdd23d4971a8bc2626d2cf32
Michał Bernacki <michber575@gmail.com>
2023-09-16 00:42:18 +0200

    echo app.env (#8)

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ echo "run db migration"
 # If Dockerfile is not run with docker-compose, DB_SOURCE env var is not defined, so we must extract env var values from the app.env
 # file so that it can be used in the migrate step here
 source /app/app.env
+cat /app/app.env
 # When ran with dokcer-composee DB_SOURCE is defined in the docker-compose.yaml and the below command will later use its value
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
